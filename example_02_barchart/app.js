d8853d23dd5e7b1f373028f194616af2022287ba
MajorX234 <majorx234@googlemail.com>
2025-12-05 23:53:34 +0100

    Add scale function and use in barchart for x,y

--- example_02_barchart/app.js
+++ example_02_barchart/app.js
@@ -1,4 +1,4 @@
-var data = [5,9,21,17,10,3,26,9,15];
+var data = [5,9,21,17,10,3,26,9,15,22,11];
 
 // SVG properties
 var barchart_width = 600;
@@ -12,20 +12,23 @@ var svg = d3.select("#barchart")
 
 // create scales
 var scale_x = d3.scaleBand()
-                .domain(d3.range(data.length)) // better idex than array
+                .domain(d3.range(data.length)) // better index than array
                 .range([0,barchart_width]);
+var scale_y = d3.scaleLinear()
+                .domain([0, d3.max(data)])
+                .range([0, barchart_height]);
 svg.selectAll('rect')
    .data(data)
-   .enter()       // for elements in data
-   .append('rect')
+   .enter()       // join data with 'rect' elements
+   .append('rect') // create missing elements (for elements in data)
    .attr('x', function(d, i){  // d is index of data array
-       return i*(barchart_width /  data.length);
+       return scale_x(i) + barchart_padding/2;
     })
    .attr( 'y', function(d){
-       return barchart_height - d*5;
+       return barchart_height - scale_y(d);
    })
-   .attr( 'width', (barchart_width/data.length) - barchart_padding)
-   .attr( 'height', function(d){ return 5*d;})
+   .attr( 'width', scale_x.bandwidth() - barchart_padding)
+   .attr( 'height', function(d){ return scale_y(d);})
    .attr('fill', 'grey'); // bars are gey
 
 svg.selectAll('text')
@@ -34,11 +37,11 @@ svg.selectAll('text')
    .append('text')
    .text(function(d){return d;}) // number in the array of data
    .attr( 'x', function (d,i){
-       return i* (barchart_width/data.length) +
-                 (barchart_width/data.length - barchart_padding) / 2
+       return scale_x(i) +
+                (scale_x.bandwidth() + barchart_padding/2) / 2
     })
    .attr( 'y', function(d){
-        return barchart_height - (d*5 - barchart_text_size );
+        return barchart_height - (scale_y(d) - barchart_text_size );
    })
    .attr( 'font-size', barchart_text_size)
    .attr( 'fill', 'black')
